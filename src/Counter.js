de0fba5268fe46a4cd84b1d6c7f83103e3df04bd
Ahmed Fahmy <afahmy31@gmail.com>
2018-09-10 16:30:28 +0200

    Add decrement button

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 
 function Counter(props) {
+  const styles = {
+    marginLeft: "10px"
+  };
   return (
     <div>
       <h1>Counter app</h1>
       <p>Count: {props.count}</p>
       <button onClick={props.onIncrementClick}>increment</button>
+      <button style={styles} onClick={props.onDecrementClick}>
+        decrement
+      </button>
     </div>
   );
 }
@@ -21,8 +27,11 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     onIncrementClick: () => {
-      console.log("click");
       const action = { type: "INCREMENT" };
+      dispatch(action);
+    },
+    onDecrementClick: () => {
+      const action = { type: "DECREMENT" };
       dispatch(action);
     }
   };
